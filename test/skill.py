6024e992998d034ea66ce05a31dc1ddea477bd23
Aoyama Nanami <aoyama_nanami@sakurasou>
2019-04-20 02:36:57 +0800

    add more tests

--- test/skill.py
+++ test/skill.py
@@ -58,5 +58,91 @@ class TestSkillData(unittest.TestCase):
             147,
             effect.DamageReduction(duration=3, percentage=50))
 
+        # スサノオ
+        self.assert_skill_equal(
+            136,
+            effect.DamageReduction(duration=5, percentage=50))
+
+        # 奔放の緑龍契士・シルヴィ
+        self.assert_skill_equal(
+            3770,
+            effect.AllOrbChange(orbs=[Orb.WOOD, Orb.LIGHT, Orb.HEART]),
+            effect.ReduceCooldown(turn=[1, 1]))
+
+        # ダルシム
+        self.assert_skill_equal(
+            5107,
+            effect.Recovery(5, 0, 0, 0, 5),
+            effect.DoubleOrbChange(Orb.LIGHT, Orb.WATER, Orb.DARK, Orb.FIRE))
+
+        # 最強装備・魔砲士
+        self.assert_skill_equal(
+            3218,
+            effect.ComboIncrease(2, 1))
+
+        # 曹操
+        self.assert_skill_equal(
+            1231,
+            effect.DelayEnemyAttack(1, 0),
+            effect.OrbChange([Orb.WATER], [Orb.FIRE]))
+
+        # ヘラ・ベオーク
+        self.assert_skill_equal(
+            1188,
+            effect.AllOrbChange([Orb.WOOD]))
+
+        # ブラキオス
+        self.assert_skill_equal(
+            11,
+            effect.DamageBuff(2, [Orb.WOOD], 150),
+            effect.AtkBasedDamage(
+                element=Element.WOOD, percentage=2000, target=effect.Target.ALL))
+
+        # 炎鎚のキリコ アナザー カード
+        self.assert_skill_equal(
+            1995,
+            effect.AllOrbChange([Orb.FIRE, Orb.LIGHT, Orb.HEART]))
+
+        # 大魔女の弟子・チェルン
+        self.assert_skill_equal(
+            4996,
+            effect.ComboIncrease(1, 1),
+            effect.Unlock(),
+            effect.AllOrbChange([Orb.WATER, Orb.HEART]))
+
+        # 水の精霊王・ザパン
+        self.assert_skill_equal(
+            4353,
+            effect.OrbEnhance([Orb.WATER], unused=6),
+            effect.SkyfallEnhancedOrbs(6, 50))
+
+        # 転生アルレシャ
+        self.assert_skill_equal(
+            5044,
+            effect.OrbChange(
+                from_=[Orb.HEART, Orb.JAMMER, Orb.POISON, Orb.MORTAL_POISON],
+                to=[Orb.WATER]),
+            effect.OrbEnhance([Orb.WATER], unused=6),
+            effect.ReduceCooldown([1, 1]))
+
+        # 浄雷の赤龍契士・ガディウス
+        self.assert_skill_equal(
+            1947,
+            effect.AllOrbChange([Orb.FIRE, Orb.LIGHT, Orb.DARK, Orb.HEART]),
+            effect.ReduceCooldown([1, 1]))
+
+        # プテラス
+        self.assert_skill_equal(
+            14,
+            effect.AtkBasedDamage(
+                element=Element.LIGHT, percentage=1000, target=effect.Target.ALL))
+
+        # 響奏の愛猫神・バステト
+        self.assert_skill_equal(
+            888,
+            effect.Cleave(3),
+            effect.DamageBuff(3, [Orb.WOOD], 115))
+
+
 if __name__ == '__main__':
     unittest.main()
