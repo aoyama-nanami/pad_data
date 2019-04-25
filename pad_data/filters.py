68d2508083e39e56964cb1378dfec8d28b2c5d42
Aoyama Nanami <aoyama_nanami@sakurasou>
2019-04-25 21:22:19 +0800

    filters: replace expression template by eval
    Change-Id: Ibde11b2f94d535519a8b06097b5cef6ef86a88f9

--- pad_data/filters.py
+++ pad_data/filters.py
@@ -1,62 +1,22 @@
+from . import common
+
 class Filter:
     pass
 
 class Skill(Filter):
-    def __init__(self, cls, f):
+    def __init__(self, cls, expr):
         self._cls = cls
-        self._f = f
+        self._expr = expr
 
     def _effect_match(self, effect):
         if not isinstance(effect, self._cls):
             return False
-        return self._f.apply(effect)
+        # pylint: disable=eval-used
+        return eval(self._expr, self._GLOBALS, {'_': effect})
 
     def __call__(self, card):
         return any(self._effect_match(e) for e in card.skill.effects)
 
-class Placeholder:
-    def __init__(self, f=lambda x: x):
-        self.apply = f
-
-    def __getattr__(self, name):
-        if name.startswith('_'):
-            raise AttributeError(f'access to {name} not allowed')
-        return Placeholder(lambda x: getattr(self.apply(x), name))
-
-    @staticmethod
-    def _maybe_apply(placeholder, x):
-        return (placeholder.apply(x) if isinstance(placeholder, Placeholder)
-                else x)
-
-    # bitwise and
-    # override of binary and/or/not not allowed
-    def __and__(self, other):
-        return Placeholder(
-            lambda x: self.apply(x) and self._maybe_apply(other, x))
-
-    # bitwise or
-    def __or__(self, other):
-        return Placeholder(
-            lambda x: self.apply(x) or self._maybe_apply(other, x))
-
-    # bitwise not
-    def __not__(self):
-        return Placeholder(lambda x: not self.apply(x))
-
-    @classmethod
-    def gen_proxy(cls, funcname):
-        def proxy(self, *args):
-            return cls(lambda x: getattr(self.apply(x), funcname)(*args))
-        setattr(cls, funcname, proxy)
-
-Placeholder.gen_proxy('__lt__')
-Placeholder.gen_proxy('__le__')
-Placeholder.gen_proxy('__eq__')
-Placeholder.gen_proxy('__ne__')
-Placeholder.gen_proxy('__gt__')
-Placeholder.gen_proxy('__ge__')
-
-Placeholder.gen_proxy('__call__')
-Placeholder.gen_proxy('__getitem__')
-Placeholder.gen_proxy('__contains__')
-Placeholder.gen_proxy('__len__')
+    _GLOBALS = dict(**common.Awakening.__members__,
+                    **common.Orb.__members__,
+                    **common.Type.__members__)
--- search.py
+++ search.py
@@ -9,7 +9,6 @@ util.import_enum_members(common.Type, globals())
 Type = common.Type
 Orb = common.Orb
 Awakening = common.Awakening
-_ = filters.Placeholder()
 
 # pylint: disable=undefined-variable
 def main():
@@ -18,15 +17,16 @@ def main():
 
     cards = filter(lambda c: c.inheritable, cards)
 
-#    cards = list(filter(
-#        filters.Skill(effect.AtkNuke, _.percentage[0] >= 25000),
-#        cards))
-
     cards = list(filter(
-        filters.Skill(
-            effect.AllOrbChange,
-            _.orbs.count(FIRE) & _.orbs.count(LIGHT) & (_.orbs.__len__() <= 4)),
+        filters.Skill(effect.AtkNuke, '_.percentage[0] >= 25000'),
         cards))
+
+    # cards = list(filter(
+    #     filters.Skill(
+    #         effect.AllOrbChange,
+    #         '_.orbs.count(FIRE) and _.orbs.count(LIGHT) and '
+    #         'len(_.orbs) <= 4'),
+    #     cards))
     cards.sort(key=lambda c: c.skill.turn_min)
     for c in cards:
         c.dump()
