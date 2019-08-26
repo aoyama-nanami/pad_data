8d12974e60d239e5af2dd3a0094fd848032a6d76
Aoyama Nanami <aoyama_nanami@sakurasou>
2019-08-26 23:02:16 +0800

    data: 20190826 update
    Change-Id: Ie0d60956e29bdcefe03fcb51efd62cf15633bad3

--- search.py
+++ search.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
 from pad_data import common, database, filters, util
-from pad_data.leader_skill import effect
+from pad_data.leader_skill import effect as ls_effect
+from pad_data.active_skill import effect as as_effect
 
 util.import_enum_members(common.Awakening, globals())
 util.import_enum_members(common.Orb, globals())
@@ -13,37 +14,39 @@ Awakening = common.Awakening
 
 def ehp(c):
     ret = 1
-    for e in c.leader_skill.effects:
-        if not isinstance(e, effect.BaseStatBoost):
+    for e in c.leader_skill.ls_effects:
+        if not isinstance(e, ls_effect.BaseStatBoost):
             continue
-        if isinstance(e, (effect.HpAbove, effect.HpBelow)):
+        if isinstance(e, (ls_effect.HpAbove, ls_effect.HpBelow)):
             continue
-        ret *= e.effective_hp()
+        ret *= e.ls_effective_hp()
     return ret
 
 def atk(c):
     ret = 1
-    for e in c.leader_skill.effects:
-        if not isinstance(e, effect.BaseStatBoost):
+    for e in c.leader_skill.ls_effects:
+        if not isinstance(e, ls_effect.BaseStatBoost):
             continue
-        if isinstance(e, (effect.HpAbove, effect.HpBelow)):
+        if isinstance(e, (ls_effect.HpAbove, ls_effect.HpBelow)):
             continue
 
-        if isinstance(e, effect.SteppedStatBoost) and e.atk_step > 0:
+        if isinstance(e, ls_effect.SteppedStatBoost) and e.atk_step > 0:
             ret *= (e.atk + e.max_step() * e.atk_step) / 100
         else:
             ret *= e.atk / 100
     return ret
 
 def board7x6(c):
-    return any(isinstance(e, effect.Board7x6) for e in c.leader_skill.effects)
+    return any(isinstance(e, ls_effect.Board7x6) for e in c.leader_skill.effects)
 
 # pylint: disable=undefined-variable
 def main():
     db = database.Database()
     cards = db.get_all_released_cards()
 
-    cards = list(filter(lambda c: ehp(c) >= 1.5 and board7x6(c), cards))
+    # cards = list(filter(lambda c: ehp(c) >= 1.5 and board7x6(c), cards))
+
+    cards = list(filter(filters.Skill(as_effect.AtkNuke, '_.value >= 100000'), cards))
 
     # cards = list(filter(
     #     filters.Skill(
@@ -51,9 +54,8 @@ def main():
     #         '_.orbs.count(FIRE) and _.orbs.count(LIGHT) and '
     #         'len(_.orbs) <= 4'),
     #     cards))
-    cards.sort(key=atk)
     for c in cards:
-        c.dump(print_leader_skill=True, print_active_skill=False)
+        c.dump(print_leader_skill=0, print_active_skill=1)
 
 if __name__ == '__main__':
     main()
