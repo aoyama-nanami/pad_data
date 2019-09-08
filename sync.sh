ac5374bb5d3d67b733e2385258dcc051b3977168
Ting Shen <phoenixshen@chromium.org>
2019-09-08 00:18:06 +0000

    sync.sh: enhanced functionality
    - allow script to be executed in arbitrary path
    - sync jp files only to reduce transfer time
    - automatically calls dump_merged_json and creates new commit
    
    Change-Id: I3bfdd4084ef12d0cf27c5f528c850ff9eb1a69f9

--- sync.sh
+++ sync.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -ex
 
-mkdir -p data/raw
+cd "$(dirname "$0")"
+
+mkdir -p data/raw/jp
 mkdir -p data/processed
-gsutil -m rsync -r -c gs://mirubot-data/paddata/raw data/raw
-gsutil -m rsync -r -c gs://mirubot-data/paddata/processed data/processed
+gsutil -m rsync -r -c gs://mirubot-data/paddata/raw/jp data/raw/jp
+gsutil -m rsync -r -c -x '(?!jp_).*' \
+    gs://mirubot-data/paddata/processed data/processed
+
+scripts/dump_merged_json.py
+
+git commit -m "data: $(date +%Y%m%d) update" -- html/data/jp_cards_merged.json
+# TODO: determine if it's better to have auto push
