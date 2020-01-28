a68342d02fbe4ba9a184c7ec337f24a89cde6bc3
Aoyama Nanami <aoyama_nanami@sakurasou>
2020-01-28 15:53:25 +0800

    html: fix incorrect type icons
    Change-Id: I38a726ab178bd9ba4884927c8a6c4a32f94b57f7

--- html/images/crop.py
+++ html/images/crop.py
@@ -21,7 +21,7 @@ for i in range(16):
     if i <= 8:
         x, y = 576, 188 + 36 * i
     else:
-        x, y = 616, 108 + 36 * (i - 9)
+        x, y = 616, 108 + 36 * (i - 12)
 
     subprocess.run(['convert', 'eggs.png', '-crop',
                     f'32x32+{x}+{y}', f't{i}.png'],
