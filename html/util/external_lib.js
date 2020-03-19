cd1cbc2e053a7fe753a88e7479d9d9c87b824912
Aoyama Nanami <aoyama_nanami@sakurasou>
2020-03-19 19:25:20 +0800

    html: update external deps
    Change-Id: I2987cf5cd70e5d0308eca468dfe6930608749a3f

--- html/util/external_lib.js
+++ html/util/external_lib.js
@@ -1,5 +1,5 @@
 import {LitElement, html, css} from 'https://unpkg.com/lit-element@2.1.0/lit-element.js?module';
-import {directive, AttributePart} from 'https://unpkg.com/lit-html@1.1.2/lit-html.js?module';
+import {directive, AttributePart} from 'https://unpkg.com/lit-html@1.2.0/lit-html.js?module';
 
 export {LitElement, html, css};
 export {directive, AttributePart};
