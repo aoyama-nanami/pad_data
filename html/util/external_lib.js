87db735faf99cff74fabcfe9f82c407cb45606dc
Aoyama Nanami <aoyama_nanami@sakurasou>
2020-03-21 04:05:46 +0800

    html: update external deps (again), add version check
    Change-Id: Ibf55d9e633426ada20b58d728df48b372e18b478

--- html/util/external_lib.js
+++ html/util/external_lib.js
@@ -1,5 +1,8 @@
 import {LitElement, html, css} from 'https://unpkg.com/lit-element@2.1.0/lit-element.js?module';
-import {directive, AttributePart} from 'https://unpkg.com/lit-html@1.2.0/lit-html.js?module';
+import {directive, AttributePart} from 'https://unpkg.com/lit-html@^1.0.0/lit-html.js?module';
+
+console.assert(new Set(litElementVersions).size == 1, litElementVersions);
+console.assert(new Set(litHtmlVersions).size == 1, litHtmlVersions);
 
 export {LitElement, html, css};
 export {directive, AttributePart};
